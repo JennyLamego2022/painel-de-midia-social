a798ce85c5c9e986975b92b1411f10f9f2a4b47c
JennyLamego2022 <jennylamego@hotmail.com>
2022-12-15 17:23:27 -0300

    feat: Criação do theme dark

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const button = document.querySelector('.js-theme')
+
+const themes = {
+    't-dark' : 't-light',
+    't-light' : 't-dark', 
+}
+
+if(button){
+    button.addEventListener('click', event => {
+        event.preventDefault()
+        const currentThemes = document.body.dataset.theme
+        document.body.setAttribute('data-theme', themes[currentThemes] || 't-light')
+    })
+}
